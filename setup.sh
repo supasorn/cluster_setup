e7f09e5df1d7da79e13d1c6166fbd7fd9ff84c6c
supasorn <supasorn@gmail.com>
2023-04-29 17:53:32 +0000

    auto commit

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ for i in $node_list; do
   if ! grep -q "^$ip " /etc/hosts; then
     sudo sh -c "echo \"$ip v${i}\" >> /etc/hosts"
   fi
-  # sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v$i"'$/,/^$/d;/^$/d' > tmpfile && mv tmpfile ~/.ssh/config
-  sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v1"'$/,/^$/d;/^$/d' 
+  sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v$i"'$/,/^$/d;/^$/d' > ~/.ssh/config
+  # sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v1"'$/,/^$/d;/^$/d' 
 done
 
 # ssh-keygen if haven't done so. 
